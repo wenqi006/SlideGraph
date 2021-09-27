595239aedb66cbc03d90d63ee936962373dc8919
wenqi006 <58427109+wenqi006@users.noreply.github.com>
2021-09-27 20:52:56 +0100

    Update features_to_graph.py

--- features_to_graph.py
+++ features_to_graph.py
@@ -1,22 +1,10 @@
 import numpy as np
-from glob import glob
 import os
-import random
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay, KDTree
 from collections import defaultdict
-from xlrd import open_workbook
 import torch
-import torch.optim as optim
 from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
 from torch_geometric.data import Data
-from scipy.spatial import Delaunay, KDTree
-from collections import defaultdict
 from scipy.cluster.hierarchy import fcluster
 from scipy.cluster import hierarchy
 from sklearn.neighbors import KDTree as sKDTree
@@ -31,46 +19,7 @@ def cuda(v):
 def toTensor(v,dtype = torch.float,requires_grad = True): 
     device = 'cuda:1'   
     return (Variable(torch.tensor(v)).type(dtype).requires_grad_(requires_grad)).to(device)
-def toNumpy(v):
-    if USE_CUDA:
-        return v.detach().cpu().numpy()
-    return v.detach().numpy()
-#%%
-def loadHoVer(flist):
-    Z = []
-    for f in flist:
-        D = np.load(f, allow_pickle=True)
-        if 'type' not in D or not len(D['type']):
-            continue
-        CT = np.hstack((np.atleast_2d(D['type']).T,D['centroid'],[[sum(sum(tup))] for tup in D['mask']]))
-        if len(Z)==0:
-            Z = CT
-        else:
-            Z = np.vstack((Z,CT))
-    return Z # combine
-#%%
-def spatialClustering(X,dthresh = 500, npts = 10e3):         
-    idx = list(range(X.shape[0]))
-    random.shuffle(idx)
-    idx = idx[:int(npts)] # selected points to avoid memory problems
-    Xtr = X[idx,:]
-    Ytr = AgglomerativeClustering(n_clusters=None,distance_threshold=dthresh,linkage = 'average').fit_predict(Xtr)
-    C = KNeighborsClassifier(n_neighbors=1).fit(Xtr,Ytr).predict(X)
-    return C # cluster id of each cell
 
-
-def clusterFeatures(Z,C):
-# Get features from nuclei types and area size
-    cids = list(set(C))
-    Fc = np.zeros((len(cids), 6))
-    Cc = np.zeros((len(cids), 2))
-    for i, c in enumerate(cids):
-        idx = C == c
-        u, counts = np.unique(Z[idx, 0], return_counts=True)
-        Fc[i, np.asarray(u, dtype=int)] = counts
-        cc = np.mean(Z[idx, 1:3], axis=0)
-        Cc[i, :] = cc
-    return Fc, Cc  # feature and coordinate average of each cluster
 
 def connectClusters(Cc,dthresh = 3000):
     tess = Delaunay(Cc)
@@ -91,28 +40,10 @@ def connectClusters(Cc,dthresh = 3000):
 
 def toGeometric(X,W,y,tt=0):    
     return Data(x=toTensor(X,requires_grad = False), edge_index=(toTensor(W,requires_grad = False)>tt).nonzero().t().contiguous(),y=toTensor([y],dtype=torch.long,requires_grad = False))
-#%%
-def getGraph(flist,y):
-    Z = loadHoVer(flist)
-    # plt.figure();plotit(np.array([[1,-1,0]])*Z[:,::-2],Z[:,0],colors = 'random', markers = 's')
-    C = spatialClustering(X = Z[:,1:3],dthresh = 500)
-    # plt.figure();plotit(Z[:,1:],C,colors = 'random')
-    Fc,Cc = clusterFeatures(Z,C)
-    W = connectClusters(Cc,dthresh = 4000)
-    # plt.figure(); plotGraph(Cc,W)
-    G = toGeometric(Fc,W,y = y)
-    G.coords = toTensor(Cc,requires_grad = False)
-    return G
-#%%
-import csv
-def loadLabels(fname):
-    D={}
-    with open(fname,'r') as f:
-        red=csv.DictReader(f)
-        c,y,m = red.fieldnames
-        for d in red:
-            D.setdefault(d[c],[]).append(d[y])
-    return D
+
+
+
+
 
 if __name__ == '__main__':
     feature_path = './hover_morphological_features' # load x, y coordinates and features of patches in each WSI
@@ -189,7 +120,3 @@ if __name__ == '__main__':
 
             with open(ofile, 'wb') as f:
                 pickle.dump(G, f)
-
-
-
-   
